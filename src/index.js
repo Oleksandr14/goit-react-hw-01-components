07cf659ee60b166434885fa5624cb20de4083b27
Oleksandr Palamarchuk <palamarchuk.oa93@gmail.com>
2022-06-26 22:36:36 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { App } from 'components/App';
+// import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+//=============================================================== 
+import ReactDOM from "react-dom/client";
+import App from "./components/App"
+
+//===========================================================================================
+// const elem1 = React.createElement('span', { children: 'Hello' });
+// const elem2 = React.createElement('span', { children: 'World' });
+
+// const element = React.createElement('div', {a:5, b:10, children: 'Hello World'});
+// console.log(element);
+
+// const jsxElement = <div>Hello World</div>;
+// ReactDOM.createRoot(document.getElementById("root")).render(element);
+//========================================================================================
+
+// const elem1 = <span>Hello</span>;
+// const elem2 = <span>World</span>;
+
+// const jsxElement = <div>{elem1}{ elem2}</div>;
+// console.log(jsxElement);
